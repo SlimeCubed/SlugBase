567f4a4d2b8e47500995d66225c5ac209d39befc
SlimeCubed <cubedslime@gmail.com>
2021-03-01 02:34:57 -0800

    Add AutoUpdate support

--- SlugBase/Properties/AssemblyInfo.cs
+++ SlugBase/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SlugBase")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A framework that simplifies adding new characters to Rain World.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Slime_Cubed")]
 [assembly: AssemblyProduct("SlugBase")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
